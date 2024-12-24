1977ad7a8024bdea92edd358f064c918205cf767
Sumit6307 <sumitkesar6307@gmail.com>
2024-12-24 19:35:30 +0530

    Add files via upload

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -67,4 +67,32 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await  bcrypt.compare(password, this.password)
 }
 
+
+  userSchema.methods.generateAccessToken = function() {
+     return  jwt.sign({
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn : process.env.ACCESS_TOKEN_EXPITY
+      }
+     )
+  }
+  userSchema.methods.generateRefreshToken = function() {
+    return  jwt.sign({
+        _id : this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn : process.env.REFRESH_TOKEN_SECRET
+      }
+     )
+  }
+
+
+
+
 export const User = mongoose.model("User", userSchemaserSchema)
