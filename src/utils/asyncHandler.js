4316a68991f8967b23f19308d86853d6dfeee987
Sumit6307 <sumitkesar6307@gmail.com>
2024-12-27 10:54:52 +0530

    Add files via upload

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,7 +3,7 @@
    
 const asyncHander = (requestHandler) => {
 
-    (req,res,next) => {
+   return  (req,res,next) => {
           Promise.resolve(requestHandler(req,res,next)).
           catch((err) => next(err))
     }
