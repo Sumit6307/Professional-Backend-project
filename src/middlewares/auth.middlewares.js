64c672eeccf8c1202c0277499428a6cd5257ead1
Sumit6307 <sumitkesar6307@gmail.com>
2024-12-29 17:26:48 +0530

    Adding Auth Features

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,3 +1,34 @@
+import {asyncHandler} from "../utils/asyncHandler";
+import {ApiError} from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models";
 
 
-export const verifyJWT
+export const verifyJWT = asyncHandler(async (req,res, next) => {
+   try {
+     const token =  req.cookies?.accessToken || req.header
+     ("Authorization")?.replace("Bearer ", "")
+ 
+     if(!token ) {
+           throw new ApiError(401, "Unauthorized Request")
+     }
+ 
+   const decodedToken =  jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+ 
+ 
+     const user =  await User.findById(decodedToken?._id).select
+    ("-password -refreshToken")
+ 
+ 
+      if(!user) {
+           throw new ApiError(401, "Invalid Access Token")
+      }
+ 
+     req.user = user;
+     next()
+
+   } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid Access Token"  )
+   }
+
+})
